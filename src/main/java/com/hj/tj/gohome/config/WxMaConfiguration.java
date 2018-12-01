bf0dd41a4af766e66ebad92da977056525a78945
woshuowozhihuiduiniyixinyiyi <562981207@qq.com>
2018-12-01 21:55:54 +0800

    修改微信配置

--- src/main/java/com/hj/tj/gohome/config/WxMaConfiguration.java
+++ src/main/java/com/hj/tj/gohome/config/WxMaConfiguration.java
@@ -34,117 +34,21 @@ import java.util.stream.Collectors;
 @EnableConfigurationProperties(WxMaProperties.class)
 @ConditionalOnClass(WxMaService.class)
 public class WxMaConfiguration {
-    private final WxMaMessageHandler templateMsgHandler = (wxMessage, context, service, sessionManager) ->
-            service.getMsgService().sendTemplateMsg(WxMaTemplateMessage.builder()
-                    .templateId("此处更换为自己的模板id")
-                    .formId("自己替换可用的formid")
-                    .data(Lists.newArrayList(
-                            new WxMaTemplateData("keyword1", "339208499", "#173177")))
-                    .toUser(wxMessage.getFromUser())
-                    .build());
-
-    private final WxMaMessageHandler logHandler = (wxMessage, context, service, sessionManager) -> {
-        System.out.println("收到消息：" + wxMessage.toString());
-        service.getMsgService().sendKefuMsg(WxMaKefuMessage.newTextBuilder().content("收到信息为：" + wxMessage.toJson())
-                .toUser(wxMessage.getFromUser()).build());
-    };
-
-    private final WxMaMessageHandler textHandler = (wxMessage, context, service, sessionManager) ->
-            service.getMsgService().sendKefuMsg(WxMaKefuMessage.newTextBuilder().content("回复文本消息")
-                    .toUser(wxMessage.getFromUser()).build());
-
-    private final WxMaMessageHandler picHandler = (wxMessage, context, service, sessionManager) -> {
-        try {
-            WxMediaUploadResult uploadResult = service.getMediaService()
-                    .uploadMedia("image", "png",
-                            ClassLoader.getSystemResourceAsStream("tmp.png"));
-            service.getMsgService().sendKefuMsg(
-                    WxMaKefuMessage
-                            .newImageBuilder()
-                            .mediaId(uploadResult.getMediaId())
-                            .toUser(wxMessage.getFromUser())
-                            .build());
-        } catch (WxErrorException e) {
-            e.printStackTrace();
-        }
-    };
-
-    private final WxMaMessageHandler qrcodeHandler = (wxMessage, context, service, sessionManager) -> {
-        try {
-            final File file = service.getQrcodeService().createQrcode("123", 430);
-            WxMediaUploadResult uploadResult = service.getMediaService().uploadMedia("image", file);
-            service.getMsgService().sendKefuMsg(
-                    WxMaKefuMessage
-                            .newImageBuilder()
-                            .mediaId(uploadResult.getMediaId())
-                            .toUser(wxMessage.getFromUser())
-                            .build());
-        } catch (WxErrorException e) {
-            e.printStackTrace();
-        }
-    };
-
     private WxMaProperties properties;
-
-    private static Map<String, WxMaMessageRouter> routers = Maps.newHashMap();
-    private static Map<String, WxMaService> maServices = Maps.newHashMap();
 
     @Autowired
     public WxMaConfiguration(WxMaProperties properties) {
         this.properties = properties;
     }
 
-    public static Map<String, WxMaMessageRouter> getRouters() {
-        return routers;
-    }
-
-    public static Map<String, WxMaService> getMaServices() {
-        return maServices;
-    }
-
-    public static WxMaService getWxMaService() {
-        Set<Map.Entry<String, WxMaService>> entries = maServices.entrySet();
-        for (Map.Entry<String, WxMaService> entry : entries) {
-            WxMaService wxMaService = entry.getValue();
-
-            return wxMaService;
-        }
-
-        return null;
-    }
-
-    @Bean
-    public Object services() {
-        maServices = this.properties.getConfigs()
-                .stream()
-                .map(a -> {
-                    WxMaInMemoryConfig config = new WxMaInMemoryConfig();
-                    config.setAppid(a.getAppid());
-                    config.setSecret(a.getSecret());
-                    config.setToken(a.getToken());
-                    config.setAesKey(a.getAesKey());
-                    config.setMsgDataFormat(a.getMsgDataFormat());
-
-                    WxMaService service = new WxMaServiceImpl();
-                    service.setWxMaConfig(config);
-                    routers.put(a.getAppid(), this.newRouter(service));
-                    return service;
-                }).collect(Collectors.toMap(s -> s.getWxMaConfig().getAppid(), a -> a));
-
-        return Boolean.TRUE;
-    }
-
     @Bean
     public WxMaConfig config() {
-        List<WxMaProperties.Config> configs = this.properties.getConfigs();
-        WxMaProperties.Config a = configs.get(0);
-
         WxMaInMemoryConfig config = new WxMaInMemoryConfig();
-        config.setAppid(a.getAppid());
-        config.setSecret(a.getSecret());
-        config.setToken(a.getToken());
-        config.setAesKey(a.getAesKey());
-        config.setMsgDataFormat(a.getMsgDataFormat());
+        config.setAppid(this.properties.getAppid());
+        config.setSecret(this.properties.getSecret());
+        config.setToken(this.properties.getToken());
+        config.setAesKey(this.properties.getAesKey());
+        config.setMsgDataFormat(this.properties.getMsgDataFormat());
 
         return config;
     }
@@ -160,17 +64,6 @@ public class WxMaConfiguration {
         WxMaService service = new WxMaServiceImpl();
         service.setWxMaConfig(configStorage);
         return service;
-    }
-
-    private WxMaMessageRouter newRouter(WxMaService service) {
-        final WxMaMessageRouter router = new WxMaMessageRouter(service);
-        router
-                .rule().handler(logHandler).next()
-                .rule().async(false).content("模板").handler(templateMsgHandler).end()
-                .rule().async(false).content("文本").handler(textHandler).end()
-                .rule().async(false).content("图片").handler(picHandler).end()
-                .rule().async(false).content("二维码").handler(qrcodeHandler).end();
-        return router;
     }
 
 }
--- src/main/java/com/hj/tj/gohome/config/WxMaProperties.java
+++ src/main/java/com/hj/tj/gohome/config/WxMaProperties.java
@@ -12,34 +12,29 @@ import java.util.List;
 @ConfigurationProperties(prefix = "wx.miniapp")
 public class WxMaProperties {
 
-    private List<Config> configs;
+    /**
+     * 设置微信小程序的appid
+     */
+    private String appid;
 
-    @Data
-    public static class Config {
-        /**
-         * 设置微信小程序的appid
-         */
-        private String appid;
+    /**
+     * 设置微信小程序的Secret
+     */
+    private String secret;
 
-        /**
-         * 设置微信小程序的Secret
-         */
-        private String secret;
+    /**
+     * 设置微信小程序消息服务器配置的token
+     */
+    private String token;
 
-        /**
-         * 设置微信小程序消息服务器配置的token
-         */
-        private String token;
+    /**
+     * 设置微信小程序消息服务器配置的EncodingAESKey
+     */
+    private String aesKey;
 
-        /**
-         * 设置微信小程序消息服务器配置的EncodingAESKey
-         */
-        private String aesKey;
-
-        /**
-         * 消息格式，XML或者JSON
-         */
-        private String msgDataFormat;
-    }
+    /**
+     * 消息格式，XML或者JSON
+     */
+    private String msgDataFormat;
 
 }
