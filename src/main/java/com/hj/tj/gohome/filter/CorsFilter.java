09cb6c41e60b67a03fe78524ddbcaead9d2b89ec
SWJ\tangj <tangj@3vjia.com>
2019-01-03 10:04:58 +0800

    修改跨域处理器

--- src/main/java/com/hj/tj/gohome/filter/CorsFilter.java
+++ src/main/java/com/hj/tj/gohome/filter/CorsFilter.java
@@ -3,6 +3,8 @@ package com.hj.tj.gohome.filter;
 import com.hj.tj.gohome.utils.StringUtil;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -35,6 +37,12 @@ public class CorsFilter implements Filter {
             throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            response.setStatus(HttpStatus.OK.value());
+            return;
+        }
+
         String originHeader = request.getHeader(HttpHeaders.ORIGIN);
         if (StringUtil.isBlank(originHeader)) {
             originHeader = "*";
@@ -42,6 +50,7 @@ public class CorsFilter implements Filter {
 
         response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, originHeader);
         response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        response.addHeader(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.AUTHORIZATION);
         response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_HEADERS);
         response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_METHODS);
         response.addHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE, DEFAULT_MAX_AGE);
